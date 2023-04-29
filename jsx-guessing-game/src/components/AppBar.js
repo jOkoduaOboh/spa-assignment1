ca59d94e321c8f51cbf6c00c632562de61c286dc
Prince John Okodua-Oboh <pro409@g.harvard.edu>
2023-04-29 12:52:31 -0400

    Completed Guessing game

---
+++ jsx-guessing-game/src/components/AppBar.js
@@ -0,0 +1,97 @@
+import styled from "@emotion/styled";
+import { useNavigate} from "react-router-dom";
+
+const TopBar = () => {
+    const navigate = useNavigate();
+    const handleMouseEnter = (e) => {
+        e.target.style.backgroundColor = 'gray';
+    };
+
+    const handleMouseLeave = (e) => {
+        e.target.style.backgroundColor = 'inherit';
+    };
+
+    const Bar = styled.div`
+    background-color: #f1f1f1;
+    padding: 0.01%;
+    text-align: center;
+    height: 100px
+    `;
+
+    const SideColumn = styled.div`
+    background-color: inherit;
+    float: left;
+    width: 40%;
+    height: 100%;
+    margin: auto auto;
+    `;
+
+    const MiddleColumn = styled.div`
+    background-color: inherit;
+    float: left;
+    width: 20%;
+    height: 100%;
+    margin: auto auto;
+    `;
+
+    const Button = styled.button`
+    height: 98%;
+    width: 42%;
+    font-size: 2.5vw;
+    margin: auto auto;
+    border: 0px;
+    border-left: 1px solid;
+    border-right: 1px solid;
+    cursor: pointer;
+    `;
+
+    const Title = styled.button`
+    height: 98%;
+    width: 100%;
+    text-align: center;
+    border: 0px;
+    font-size: 140%;
+    font-weight: bold;
+    `;
+
+    return (
+        <>
+            <Bar>
+                <SideColumn>
+                    <Button
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                        onClick={() => { navigate("/") }}
+                    >
+                        Play
+                    </Button>
+                </SideColumn>
+                <MiddleColumn>
+                    <Title>Guessing Game</Title>
+                </MiddleColumn>
+                <SideColumn>
+                    <Button
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                        onClick={() => { navigate("/stats") }}
+                    >
+                        Statistics
+                    </Button>
+                    <Button
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                        onClick={() => { navigate("/settings") }}
+                    >
+                        Settings
+                    </Button>
+                </SideColumn>
+            </Bar>
+        </>
+
+    )
+}
+
+
+
+export default TopBar;
+
---
+++ jsx-guessing-game/src/pages/Home.js
@@ -0,0 +1,141 @@
+import { useState } from "react";
+import { getMaxGuessVal, getMinGuessVal, getNumOfGuesses } from "../components/SettingValues";
+import { updateGuessTendency, updateGuessesPerWin, updateTotalGuesses, updateWins } from "../components/Stat";
+import styled from "@emotion/styled";
+
+const Home = () => {
+    const [firstRender, setFirstRender] = useState(true)
+    const [guessesLeft, setGuessesLeft] = useState(getNumOfGuesses());
+
+    const [valueToGuess, setValueToGuess] = useState(0);
+
+    const [closestLowerGuess, setClosestLowerGuess] = useState(getMinGuessVal());
+    const [closestHigherGuess, setClosestHigherGuess] = useState(getMaxGuessVal());
+
+    const [notified, setNotified] = useState(true)
+    const [gameOver, setGameOver] = useState(false);
+    const [resultdisplay, setResultDisplay] = useState(false);
+    const [isDebug, setIsDebug] = useState(false);
+
+    const [notificationMsg, setNotificationMsg] = useState("Too High");
+    const [resultMsg, setResultMsg] = useState("Too High");
+
+    if (firstRender) {
+        setFirstRender(false)
+        setValueToGuess(Math.floor(Math.random() * (getMaxGuessVal() - getMinGuessVal() + 1)) + getMinGuessVal());
+    }
+
+    const handleKeyUp = (e) => {
+        if (e.key === "Enter") {
+            const guessedVal = Number(e.target.value)
+            console.log('Guessed Val: ', guessedVal)
+            setGuessesLeft(guessesLeft - 1);
+
+            if (guessedVal === valueToGuess) {
+                setResultMsg("You Win!");
+                setResultDisplay(true)
+                updateWins()
+                updateTotalGuesses(getNumOfGuesses() - (guessesLeft - 1))
+                updateGuessesPerWin()
+                e.target.disabled = true;
+                setGameOver(true);
+            } else if (guessesLeft > 0) {
+                console.log("Heree")
+                if (guessedVal > valueToGuess) {
+                    updateGuessTendency(1);
+                    if (guessedVal < closestHigherGuess)
+                        setClosestHigherGuess(guessedVal)
+                    console.log("Too High")
+                    setNotificationMsg("Too High");
+                }
+                else {
+                    updateGuessTendency(-1);
+                    if (guessedVal > closestLowerGuess)
+                        setClosestLowerGuess(guessedVal)
+                    console.log("Too Low")
+                    setNotificationMsg("Too Low");
+                }
+                e.target.value = "";
+                setNotified(false)
+
+                setTimeout(() => {
+                    setNotified(true)
+                }, 1500);
+            }
+            if (guessesLeft === 1 && gameOver == false) {
+                updateTotalGuesses(getNumOfGuesses() - (guessesLeft - 1))
+                updateGuessesPerWin()
+                e.target.disabled = true;
+                setResultMsg("You Lose!");
+                setResultDisplay(true)
+                setGameOver(true)
+            }
+        }
+    }
+
+    const handlePlayAgain = () => {
+        setGuessesLeft(getNumOfGuesses())
+        setClosestHigherGuess(getMaxGuessVal())
+        setClosestLowerGuess(getMinGuessVal())
+        setValueToGuess(Math.floor(Math.random() * (getMaxGuessVal() - getMinGuessVal() + 1)) + getMinGuessVal());
+        setResultDisplay(false)
+        setGameOver(false)
+        document.getElementById("numInput").value = "";
+        document.getElementById("numInput").disabled = false;
+        console.log("Play again")
+    }
+
+    const handleDebug = () => {
+        setIsDebug(!isDebug);
+    }
+
+    const Space = styled.span`
+        display:inline-block; 
+        width: 6vw;
+    `
+
+    const Arrow = styled.h1`
+        margin: 0;
+        display:inline-block; 
+    `
+    const Range = styled.h2`
+    margin: 0;
+    display:inline-block; 
+    font-weight:normal;
+    `
+
+    return (
+        <div>
+            <button onClick={handleDebug}>Debug</button>
+            {isDebug === true &&
+                <div>Number to guess: {valueToGuess}</div>}
+
+            <div>
+                <p></p>
+                <strong>Guess the Number</strong>
+            </div>
+            <div>Guesses Left: {guessesLeft}
+                <p></p>
+            </div>
+
+            <input id="numInput" type="number" onKeyUp={handleKeyUp} />
+
+            <div>
+                <Range> {closestLowerGuess} </Range> &nbsp; <Arrow> &#8594; </Arrow>
+                <Space /> <Range><strong>Between</strong></Range> <Space />
+                <Arrow> &#8592; </Arrow> &nbsp; <Range> {closestHigherGuess} </Range>
+                <div></div>
+            </div>
+
+            {notified === false &&
+                <div>{notificationMsg}</div>}
+            {resultdisplay === true &&
+                <div>{resultMsg}</div>}
+            {gameOver === true && <>
+                <button onClick={handlePlayAgain}>Play Again</button>
+            </>}
+        </div>
+    )
+}
+
+export default Home;
